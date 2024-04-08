ab00b42bbacdfe91823c4c6fa0caccfbb3f54335
Mark Leetch <markleetch@gmail.com>
2024-04-09 00:22:11 +0100

    Added progress indicator, & better error handling

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::fs;
 use configparser::ini::Ini;
 use std::io::{self, Read};
 use clap::Parser;
+use encoding_rs::UTF_8;
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
+use std::time::Duration;
+use std::error::Error;
 
 // TODO: Accept & parse an actual .setlist file
 // TODO: Add some tests
@@ -31,7 +36,7 @@ impl Song {
   fn from_ini(ini: Ini, hash: String) -> Song {
     let artist = ini.get("song", "artist").unwrap();
     let name = ini.get("song", "name").unwrap();
-    let charter = ini.get("song", "charter").unwrap();
+    let charter = ini.get("song", "charter").unwrap_or("Unknown Charter".to_string());
     let hash = hash.to_uppercase();
     Song {
       artist,
@@ -79,25 +84,51 @@ fn process_charts(charts_path: &str) -> Vec<Song> {
     .unwrap();
   let charts = walker.filter_map(|result| result.ok());
 
+  let spinner = ProgressBar::new_spinner();
+  spinner.enable_steady_tick(Duration::from_millis(100));
+  spinner.set_style(
+    ProgressStyle::default_spinner()
+      .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈⠈")
+  );
+
   for chart in charts {
     let chart_path = chart.path();
     let song_ini_path = chart.path().parent().unwrap().display().to_string() + "/song.ini";
-    let song_ini = parse_ini(&song_ini_path);
-    let chart_hash = calculate_md5_hash(chart_path.to_str().unwrap());
 
-    match chart_hash {
-      Ok(hash) => chart_list.push(Song::from_ini(song_ini, hash)),
-      Err(e) => panic!("Error calculating md5 hash: {}", e),
-    }
+    let song_ini = match parse_ini(&song_ini_path) {
+      Ok(ini) => ini,
+      Err(e) => {
+          eprintln!("Error parsing INI file: {}", e);
+          continue; // Skip to the next chart
+      }
+    };
+
+    let chart_hash = match calculate_md5_hash(chart_path.to_str().unwrap()) {
+      Ok(hash) => hash,
+      Err(e) => panic!("Error calculating MD5 hash: {}", e),
+    };
+
+    let song = Song::from_ini(song_ini, chart_hash);
+    spinner.set_message(format!("Processing: {} - {} (Charter: {})", song.artist, song.name, song.charter));
+    spinner.inc(1);
+    chart_list.push(song);
   }
   chart_list
 }
 
-fn parse_ini(file: &str) -> Ini {
-  let contents = fs::read_to_string(file).expect("Something went wrong reading the file");
+fn parse_ini(file: &str) -> Result<Ini, Box<dyn Error>> {
+  let content_bytes = fs::read(file).expect("Something went wrong reading the file");
+  let (contents, _, had_errors) = UTF_8.decode(&content_bytes);
+
+  if had_errors {
+    println!("Cannot Parse File - Non UTF-8 sequence detected: {}", file);
+  }
+
   let mut ini = Ini::new();
-  ini.read(contents).unwrap();
-  ini
+  match ini.read(contents.to_string()).map_err(|e| e.to_string()) {
+    Ok(_) => Ok(ini),
+    Err(e) => Err(format!("Error parsing ini file @ {}: {}", file, e).into()),
+  }
 }
 
 fn calculate_md5_hash(file_path: &str) -> io::Result<String> {
